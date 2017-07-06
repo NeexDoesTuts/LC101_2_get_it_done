9c68716d7d3e37d77a1eb6a865424a915c9eddba
neex <neex-io@users.noreply.github.com>
2017-07-05 22:34:58 -0700

    Add SQL information

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://get-it-done:getitdone@localhost:3306/get-it-done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
-tasks = []
 
 @app.route("/", methods=["POST", "GET"])
 def index():
@@ -13,5 +16,5 @@ def index():
 
     return render_template("todos.html", title="Get It Done!", tasks=tasks)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run()
